7a3b37b4a2f7b331acc66bc83178a02a4bf3c413
dhruvsharma1999 <dhruvshrm34@gmail.com>
2020-11-21 21:32:38 +0530

    added day 1 program

---
+++ code/D1_brandNameGen.py
@@ -0,0 +1,11 @@
+#creating a greeting for the program
+print("Welcome to the band name generator.")
+
+#asking th user for the city that they grew up in
+city = input("Which city did you grow up in?\n")
+
+#asking for the name of a pet
+pet = input("what is the name of the pet?\n")
+
+#combining the name their city and pet to show the name of their band
+print("You band name could be " + city + " " + pet)
