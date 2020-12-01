0cd290576be08fe86e2b0bd1e8ef5fb5c0f46990
dhruvsharma1999 <dhruvshrm34@gmail.com>
2020-12-01 21:28:05 +0530

    added D6

--- code/D6_coffe_main.py
+++ code/D6_coffe_main.py
@@ -1,3 +1,24 @@
 from D6_menu import Menu, MenuItem
 from D6_coffee_maker import CoffeeMaker
 from D6_money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ing = coffee_maker.is_resource_sufficient(drink)
+        is_payment_succ = money_machine.make_payment(drink.cost)
+        if is_enough_ing and is_payment_succ:
+            coffee_maker.make_coffee(drink)
