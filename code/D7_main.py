3184e44eb33c47923e1ccc0cf48e5eb0a321a019
dhruvsharma1999 <dhruvshrm34@gmail.com>
2020-12-03 22:29:02 +0530

    completed D7

--- code/D7_main.py
+++ code/D7_main.py
@@ -11,4 +11,10 @@ for question in question_data:
     
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You have completed the quiz")
+print(f"Your final score is:{quiz.score}/{quiz.question_number}")
