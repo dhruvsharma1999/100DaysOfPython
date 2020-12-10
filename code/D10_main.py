3e54d189180291d11607ddb5ff8ec2119214b346
dhruvsharma1999 <dhruvshrm34@gmail.com>
2020-12-10 21:38:04 +0530

    updated D11

--- code/D10_main.py
+++ code/D10_main.py
@@ -38,10 +38,9 @@ while game_is_on:
         game_is_on = False
         score_board.game_over()
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             score_board.game_over()
 
