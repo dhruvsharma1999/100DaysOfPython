8c7d8543d823540b62ea17d1974172f2c728ea6d
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-01-25 15:45:12 +0530

    added day 15, file handling

---
+++ code/D15/Mail Merge Project Start/main.py
@@ -0,0 +1,15 @@
+PLACEHOLDER = "[name]"
+
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_names = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_names)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_names}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
